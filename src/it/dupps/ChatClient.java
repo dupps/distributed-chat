244deaf1531ea3869e7f42c24e2a293fabda0314
Bastian Dupps <dupps@email.de>
2015-03-28 20:56:26 +0100

    chat client moved to very simple GUI interface

--- src/it/dupps/ChatClient.java
+++ src/it/dupps/ChatClient.java
@@ -4,81 +4,124 @@ package it.dupps;
  * Created by dupps on 28.03.15.
  */
 
+import java.applet.Applet;
+import java.awt.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class ChatClient implements Runnable {
+public class ChatClient extends Applet {
 
     private Socket socket = null;
-    private Thread thread = null;
     private DataInputStream console = null;
     private DataOutputStream streamOut = null;
     private ChatClientThread client = null;
+    private TextArea display = new TextArea();
+    private TextField input = new TextField();
+    private Button send = new Button("Send"), connect = new Button("Connect"),
+                   quit = new Button("Bye");
+    private String serverName = "localhost";
+    private int serverPort = 4444;
 
-    public ChatClient(String serverName, int serverPort) {
-        System.out.println("Establishing connection. Please wait ...");
+    public void init() {
+        Panel keys = new Panel();
+        keys.setLayout(new GridLayout(1, 2));
+        keys.add(quit);
+        keys.add(connect);
+        Panel south = new Panel();
+        south.setLayout(new BorderLayout());
+        south.add("West", keys);
+        south.add("Center", input);
+        south.add("East", send);
+        Label title = new Label("Simple Chat Client Applet", Label.CENTER);
+        title.setFont(new Font("Helvetica", Font.BOLD, 14));
+        setLayout(new BorderLayout());
+        add("North", title);
+        add("Center", display);
+        add("South", south);
+        quit.disable();
+        send.disable();
+        getParameters();
+    }
+
+    public boolean action(Event e, Object o) {
+        if (e.target == quit) {
+            input.setText(".bye");
+            send();
+            quit.disable();
+            send.disable();
+            connect.enable();
+        } else if (e.target == connect) {
+            connect(serverName, serverPort);
+        } else if (e.target == send) {
+            send();
+            input.requestFocus();
+        }
+        return true;
+    }
+
+    public void connect(String serverName, int serverPort) {
+        println("Establishing connection. Please wait ...");
         try {
             socket = new Socket(serverName, serverPort);
-            System.out.println("Connected: " + socket);
-            start();
+            println("Connected: " + socket);
+            open();
+            send.enable();
+            connect.disable();
+            quit.enable();
         } catch (UnknownHostException uhe) {
-            System.out.println("Host unknown: " + uhe.getMessage());
+            println("Host unknown: " + uhe.getMessage());
         } catch (IOException ioe) {
-            System.out.println("Unexpected exception: " + ioe.getMessage());
+            println("Unexpected exception: " + ioe.getMessage());
         }
     }
 
-    public void run() {
-        while (thread != null) {
-            try {
-                streamOut.writeUTF(console.readLine());
-                streamOut.flush();
-            } catch (IOException ioe) {
-                System.out.println("Sending error: " + ioe.getMessage());
-                stop();
-            }
+    private void send() {
+        try {
+            streamOut.writeUTF(input.getText());
+            streamOut.flush();
+            input.setText("");
+        } catch (IOException ioe) {
+            println("Sending error: " + ioe.getMessage());
+            close();
         }
     }
 
     public void handle(String msg) {
         if (msg.equals(".bye")) {
-            System.out.println("Good bye. Press RETURN to exit ...");
-            stop();
-        } else System.out.println(msg);
+            println("Good bye. Press RETURN to exit ...");
+            close();
+        } else println(msg);
     }
 
-    public void start() throws IOException {
-        console = new DataInputStream(System. in );
-        streamOut = new DataOutputStream(socket.getOutputStream());
-        if (thread == null) {
-            client = new ChatClientThread(this, socket);
-            thread = new Thread(this);
-            thread.start();
-        }
-    }
-
-    public void stop() {
-        if (thread != null) {
-            thread.stop();
-            thread = null;
-        }
+    public void open() {
         try {
-            if (console != null) console.close();
+            streamOut = new DataOutputStream(socket.getOutputStream());
+            client = new ChatClientThread(this, socket);
+        } catch (IOException ioe) {
+            println("Error opening output stream: " + ioe);
+        }
+    }
+
+    public void close() {
+        try {
             if (streamOut != null) streamOut.close();
             if (socket != null) socket.close();
         } catch (IOException ioe) {
-            System.out.println("Error closing ...");
+            println("Error closing ...");
         }
         client.close();
         client.stop();
     }
 
-    public static void main(String args[]) {
-        ChatClient client = null;
-        if (args.length != 2) System.out.println("Usage: java ChatClient host port");
-        else client = new ChatClient(args[0], Integer.parseInt(args[1]));
+    private void println(String msg) {
+        display.appendText(msg + "\n");
+    }
+
+    public void getParameters() {
+        serverName = getParameter("host");
+        serverPort = Integer.parseInt(getParameter("port"));
     }
 }
